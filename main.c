b8b0f42c18779515791119d2b165c14e02e38422
Ariel Drellich <73121058+ArielDrellich@users.noreply.github.com>
2021-08-11 22:54:47 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,67 @@
+#include "chess.h"
+#define _CRT_SECURE_NO_WARNINGS
+#define SIZE 8
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+
+void main() {
+	char board[SIZE][SIZE];
+	char defaultFen[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+	char fen[64];
+	int keepPlaying = 1;
+	int playOption;
+	int i = 0;
+	char pgn[9];
+	while (keepPlaying) {
+		printf("_____Welcome to Chess!_____\nTo start a new game, enter 1.\nTo resume a game, enter 2: ");
+		scanf(" %d", &playOption);
+		if (playOption == 2) {
+			printf("Please type a legal FEN to resume game: ");
+			scanf("%s", fen);
+		} else strcpy(fen, defaultFen);
+
+		createBoard(board, fen);
+		printf("_____New Game_____\n\nFor help at any point, enter \"help\".\n\n");
+
+		printBoard(board);
+		do {
+			if ((i + 1) % 2)
+				printf("_____White's turn____\nInput a legal command: ");
+			else
+				printf("_____Black's turn____\nInput a legal command: ");
+
+			scanf("%s", pgn);
+			printf("\n");
+
+			if (!strcmp(pgn, "restart")) {
+				i = 0;
+				break;
+			}
+
+			if (!strcmp(pgn, "exit") || !strcmp(pgn, "quit")) {
+				keepPlaying = 0;
+				break;
+			}
+
+			if (!strcmp(pgn, "help")) {
+				printf("%s", helpText);
+				continue;
+			}
+
+			if (!strcmp(pgn, "fen")) {
+				printFen(board);
+				continue;
+			}
+
+			if (makeMove(board, pgn, (i + 1) % 2))
+				i++;
+			else
+				printf("Illegal move/input\n\n");
+
+		} while (!makeMove(board, pgn, (i + 1) % 2));
+	}
+}
